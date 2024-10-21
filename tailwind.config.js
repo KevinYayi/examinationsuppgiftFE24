e6505b2f0a83b043f851307b9df7fca62bccc0a5
Kevin Ruangrad <kevin_9@live.se>
2024-10-21 11:23:55 +0200

    finalized 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/*.{html,js}"], // Correct placement of content
-    theme: {
-      extend: {},
-      daisyui: {
-        themes: [
-          {
-            mytheme: {
-              neutral: "#bebebe",
-            },
-          },
-        ],
+  content: ["./src/*.{html,js}"], // Defines the content paths correctly
+  theme: {
+    extend: {
+      fontFamily: {
+        dmfont: ["DM Serif Text", "sans-serif"], // Custom font
       },
     },
-    plugins: [require("daisyui"), require("taos/plugin")], // Plugins should be outside 'theme'
-    safelist: [
-      "!duration-[0ms]",
-      "!delay-[0ms]",
-      'html.js :where([class*="taos:"]:not(.taos-init))',
-    ],
-    daisyui: {
-      themes: [
-        {
-          mytheme: {
-            neutral: "#bebebe",
-          },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          neutral: "#bebebe", // Custom theme color
         },
-      ],
-    },
-    content: {
-      relative: true,
-      transform: (content) => content.replace(/taos:/g, ""),
-      files: ["./src/*.{html,js}"],
-    },
-  };
-  
+      },
+    ],
+  },
+  plugins: [
+    require("daisyui"), // Plugins are correctly placed outside 'theme'
+    require("taos/plugin"),
+  ],
+  safelist: [
+    "!duration-[0ms]",
+    "!delay-[0ms]",
+    'html.js :where([class*="taos:"]:not(.taos-init))',
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ""), // Transforms the content
+  },
+};
